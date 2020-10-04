89cf91067c3b8663ffb6326328c4a53d3cacd973
akashjagdale <adjagdale25@gmail.com>
2020-10-04 23:29:01 +0530

    Added POJOs. closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthData;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthData() {
+        return birthData;
+    }
+
+    public void setBirthData(LocalDate birthData) {
+        this.birthData = birthData;
+    }
+}
