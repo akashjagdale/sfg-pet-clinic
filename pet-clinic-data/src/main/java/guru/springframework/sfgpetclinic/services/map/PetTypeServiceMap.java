fa5ce4430e743f117b5ea8875627e50a54e93af9
akashjagdale <adjagdale25@gmail.com>
2020-10-11 21:42:14 +0530

    Created PetType Map Service Implementation. closes #30

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType deleteById(Long id) {
+        return super.deleteById(id);
+    }
+
+    @Override
+    public Boolean delete(PetType petType) {
+        return super.delete(petType);
+    }
+}
