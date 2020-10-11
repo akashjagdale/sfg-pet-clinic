6debe09cce9b9d6fc3bbd04d1ed03c97aedeb2e6
akashjagdale <adjagdale25@gmail.com>
2020-10-11 23:37:33 +0530

    Created Specialty Map Service Implementation. closes #32

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialitiesServiceMap.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialitiesServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality deleteById(Long id) {
+        return super.deleteById(id);
+    }
+
+    @Override
+    public Boolean delete(Speciality speciality) {
+        return super.delete(speciality);
+    }
+}
