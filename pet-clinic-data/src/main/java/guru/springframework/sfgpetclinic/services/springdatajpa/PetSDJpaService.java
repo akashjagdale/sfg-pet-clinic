900b422435454a3d6ef2d641365b8e4e6b2a85f2
akashjagdale <adjagdale25@gmail.com>
2021-02-14 23:38:41 +0530

    Error while loading Spring context.#57

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -4,14 +4,13 @@ import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.repositories.PetRepository;
 import guru.springframework.sfgpetclinic.services.PetService;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
-@Profile("servicedatajpa")
+//@Profile("servicedatajpa")
 @Qualifier("petSDJpaService")
 public class PetSDJpaService implements PetService {
     private final PetRepository petRepository;
