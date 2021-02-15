5ece33dbf638260af6d95f6f406ae4f60672125e
akashjagdale <adjagdale25@gmail.com>
2021-02-16 01:26:30 +0530

    Implemented Vets JSON Endpoints. closes #74

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -17,5 +22,11 @@ public class VetController {
     public String listVest(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    @ResponseBody
+    public Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
